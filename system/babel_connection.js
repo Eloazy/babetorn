81bb40967bab6bdb916e6ee71061ccb94bf93fb9
Eloazy <miyu.shako@gmail.com>
2025-03-17 22:27:55 -0300

    added re-analysis + logs

--- system/babel_connection.js
+++ system/babel_connection.js
@@ -1,17 +1,41 @@
-export default async function(query) {
+var prompt = null
+export default async function(query, tornconnection) {
 	try {
-		const response = await fetch('http://localhost:1025/api/branch/babetorn', {
-			method: 'POST',
-			headers: {
-				'Content-Type':'application/json'
-			},
-			body: JSON.stringify({ query })
-		})
-		if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
+		if(tornconnection == true) {
+			query = 'user/faction data: ' + JSON.stringify(query) + ' manipulate the data as requested in this prompt: ' + prompt
+			console.log('\x1b[35m> Analyzing TORN data\x1b[0m')
+			const response = await fetch('http://localhost:1025/api/branch/babelaife', {
+				method: 'POST',
+				headers: {
+					'Content-Type':'application/json'
+				},
+				body: JSON.stringify({ query })
+			})
+			if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
 			const data = await response.json()
 			if(!data) {throw new Error(`Error: undefined data`)}
+			console.log('\x1b[35m> Analysis finished\x1b[0m')
+			prompt = null
 			return typeof data.result == 'string' ? data.result : data.result.result
+		}
+		else {
+			prompt = query
+			console.log('\x1b[35m> Analysing request\x1b[0m')
+			const response = await fetch('http://localhost:1025/api/branch/babetorn', {
+				method: 'POST',
+				headers: {
+					'Content-Type':'application/json'
+				},
+				body: JSON.stringify({ query })
+			})
+			if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
+			const data = await response.json()
+			if(!data) {throw new Error(`Error: undefined data`)}
+			console.log('\x1b[35m> Analysis finished\x1b[0m')
+			return typeof data.result == 'string' ? data.result : data.result.result
+		}
+		
 	} catch(err) {
-		console.log('\x1b[31mERROR:',err,'\x1b[0m')
+		console.error('\x1b[31mERROR:',err.message,'\x1b[0m')
 	}
 }
--- system/torn_connection.js
+++ system/torn_connection.js
@@ -1,13 +1,17 @@
 //torn connection V1
+import babel from './babel_connection.js'
+
 export default async function(link) {
 	try {
+		console.log('\x1b[35m> Starting connection with TORN\x1b[0m')
 		const response = await fetch(link)
 		if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
 	        
 	    const data = await response.json()
 	    if(!data) {throw new Error(`Error: undefined data`)}
-	    return data
+	    console.log('\x1b[35m> Connection terminated, data acquired successfully\x1b[0m')
+	    return await babel(data, true)
 	} catch(err) {
-		console.log('\x1b[31mERROR:',err,'\x1b[0m')
+		console.error('\x1b[31mERROR:',err.message,'\x1b[0m')
 	}
 }
